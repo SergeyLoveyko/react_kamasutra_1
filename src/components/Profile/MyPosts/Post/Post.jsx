71b079ef95a68a1065d2547c22db17b72c440c1b
Sergey <loveykosergey@ukr.net>
2021-07-05 00:55:57 +0300

    first commit

---
+++ src/components/Profile/MyPosts/Post/Post.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import s from './Post.module.css';
+
+
+const Post = (props) => {
+    return  <div className={s.item}>
+                <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSxLkbtTa0kfmKizxJgqECQLdlt_xq1R2jEQQ&usqp=CAU'/>
+                { props.message }
+                
+                <div className={s.like}>
+                    <span>like</span> { props.likesCount }
+                </div>
+            </div>
+
+            
+}
+
+export default Post;
---
+++ src/redux/state.js
@@ -0,0 +1,29 @@
+let state = {
+    profilePage: {
+        posts: [
+            {id: 1, message: 'Hi, how are you ?', likesCount: 11},
+            {id: 2, message: 'It\'s my first post', likesCount: 24},
+            {id: 3, message: 'It\'s my first post', likesCount: 7},
+            {id: 4, message: 'It\'s my first post', likesCount: 3}
+        ],
+    },
+    dialogsPage: {
+        dialogs: [
+            {id: 1, name: 'Dimych'},
+            {id: 2, name: 'Andrey'},
+            {id: 3, name: 'Maria'},
+            {id: 4, name: 'Kolay'},
+            {id: 5, name: 'Sveta'},
+            {id: 6, name: 'Victor'}
+        ],
+        messages: [
+            {id: 1, message: 'Hi'},
+            {id: 2, message: 'How is your it-kamasutra ?'},
+            {id: 3, message: 'Yo'},
+            {id: 4, message: 'Hi'},
+            {id: 5, message: 'Yo'}
+        ]
+    }
+}
+
+export default state;
